6ac3e9901d8fd1f6c5331a61724e2e615e2f2924
aAmohammadrezaaA <error.2013@yahoo.com>
2023-08-29 11:57:17 +0330

    perfecting

--- more_advanced_RVSwith_tf.py
+++ more_advanced_RVSwith_tf.py
@@ -717,6 +717,7 @@ with log_writer.as_default():
       if val_loss.result()<last_val_loss:
         ckpt.save(checkpoint_path)
         last_val_loss=val_loss.result()
+        #!cp DRIVE/ckpt drive/MyDrive/Colab/vision_ds/ -Rf
     print("")
     tf.summary.scalar("train_loss", train_loss.result(), step=epoch)
     tf.summary.scalar("train_acc", train_acc.result(), step=epoch)
@@ -724,6 +725,9 @@ with log_writer.as_default():
 
 # for training from last saved checkpoint:
 '''
+#from google.colab import drive
+#drive.mount('/content/drive')
+#!cp drive/MyDrive/Colab/vision_ds/ckpt DRIVE/ -R
 ckpt.restore(tf.train.latest_checkpoint(checkpoint_path))
 start_epoch = optimizer.iterations.numpy() // (len(train_patch_img_path_list)/BATCH_SIZE) + 1
 from tensorflow.python.ops.batch_ops import batch
@@ -754,6 +758,7 @@ with log_writer.as_default():
       if val_loss.result()<last_val_loss:
         ckpt.save(checkpoint_path)
         last_val_loss=val_loss.result()
+        #!cp DRIVE/ckpt drive/MyDrive/Colab/vision_ds/ -Rf
     print("")
     tf.summary.scalar("train_loss", train_loss.result(), step=epoch)
     tf.summary.scalar("train_acc", train_acc.result(), step=epoch)

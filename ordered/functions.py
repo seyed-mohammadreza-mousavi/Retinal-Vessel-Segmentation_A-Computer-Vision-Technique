9ee659ce5ddc3a17ef49c276aec89c9cd2fad117
seyed-mohammadreza-mousavi <mohammadreza92299@gmail.com>
2023-10-15 00:19:31 +0330

    ordered code

---
+++ ordered/functions.py
@@ -0,0 +1,119 @@
+def check_coord(x,y,h,w,patch_size):
+  if x-patch_size/2>0 and x+patch_size/2<h and y-patch_size/2>0 and y+patch_size/2<w:
+    return True
+  return False
+
+def adjust_gamma(imgs, gamma=1.0):
+  invGamma = 1.0 / gamma
+  table = np.array([((i / 255.0) ** invGamma) * 255 for i in np.arange(0, 256)]).astype("uint8")
+  # apply gamma correction using the lookup table
+  new_imgs = np.empty(imgs.shape)
+  for i in range(imgs.shape[2]):
+    new_imgs[:,:,i] = cv2.LUT(np.array(imgs[:,:,i], dtype = np.uint8), table)
+  return new_imgs
+
+# CLAHE (Contrast Limited Adaptive Histogram Equalization)
+#adaptive histogram equalization is used. In this, image is divided into small blocks called "tiles" (tileSize is 8x8 by default in OpenCV). Then each of these blocks are histogram equalized as usual. So in a small area, histogram would confine to a small region (unless there is noise). If noise is there, it will be amplified. To avoid this, contrast limiting is applied. If any histogram bin is above the specified contrast limit (by default 40 in OpenCV), those pixels are clipped and distributed uniformly to other bins before applying histogram equalization. After equalization, to remove artifacts in tile borders, bilinear interpolation is applied
+def clahe_equalized(imgs):
+  clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+  imgs_equalized = np.empty(imgs.shape)
+  for i in range(imgs.shape[2]):
+    imgs_equalized[:,:,i] = clahe.apply(np.array(imgs[:,:,i], dtype = np.uint8))
+  return imgs_equalized
+
+def restrict_normalized(imgs,mask):
+    imgs_normalized = np.empty(imgs.shape)
+    imgs_std = np.std(imgs)
+    imgs_mean = np.mean(imgs)
+    imgs_normalized = (imgs-imgs_mean)/imgs_std
+    for i in range(imgs.shape[2]):
+        imgs_normalized[:,:,i] = ((imgs_normalized[:,:,i] - np.min(imgs_normalized[:,:,i])) / (np.max(imgs_normalized[:,:,i])-np.min(imgs_normalized[:,:,i])))*255
+    return imgs_normalized
+
+def preprocess(image,mask):
+
+  assert np.max(mask)==1
+  image=np.array(image)
+  image[:,:,0]=image[:,:,0]*mask
+  image[:,:,1]=image[:,:,1]*mask
+  image[:,:,2]=image[:,:,2]*mask
+
+  image=restrict_normalized(image,mask)
+  image=clahe_equalized(image)
+  image=adjust_gamma(image,1.2)
+  image=image/255.0
+  return image
+
+def image2patch_train(image_path,patch_num,patch_size,training=True,show=True):
+  image_name=image_path.split("/")[-1].split("_")[0]    # 25 for first image
+
+  image=plt.imread(image_path)
+
+  #groundtruth=plt.imread(train_groundtruth_dir+image_name+"_manual1.gif")
+  groundtruth=plt.imread(train_dir+"1st_manual/"+image_name+"_manual1.gif")
+  groundtruth=np.where(groundtruth>0,1,0)
+
+  mask=plt.imread(train_mask_dir+image_name+"_training_mask.gif")
+  mask=np.where(mask>0,1,0)
+
+  image=preprocess(image,mask)
+  #image_binary=0.8*image[:,:,1]+0.2*image[:,:,2]
+
+  image_show=image.copy()
+  groundtruth_show=np.zeros_like(image)
+  groundtruth_show[:,:,0]=groundtruth.copy()
+  groundtruth_show[:,:,1]=groundtruth.copy()
+  groundtruth_show[:,:,2]=groundtruth.copy()
+
+  sample_count=0
+  sample_index=0
+
+  sample_point=np.where(groundtruth==1)     # generate sample point
+
+  state = np.random.get_state()      # shuffle the coord
+  np.random.shuffle(sample_point[0])
+  np.random.set_state(state)
+  np.random.shuffle(sample_point[1])
+
+  patch_image_list=[]
+  patch_groundtruth_list=[]
+
+  while sample_count<patch_num and sample_index<len(sample_point[0]):
+    x,y=sample_point[0][sample_index],sample_point[1][sample_index]
+    if check_coord(x,y,image.shape[0],image.shape[1],patch_size):
+      if np.sum(mask[x-patch_size//2:x+patch_size//2,y-patch_size//2:y+patch_size//2])>patch_threshold:     #select according to the threshold
+
+        patch_image_binary=image[x-patch_size//2:x+patch_size//2,y-patch_size//2:y+patch_size//2,:]   # patch image
+        patch_groundtruth=groundtruth[x-patch_size//2:x+patch_size//2,y-patch_size//2:y+patch_size//2]       # patch mask
+        #patch_image_binary=np.asarray(0.25*patch_image[:,:,2]+0.75*patch_image[:,:,1])         # B*0.25+G*0.75, which enhance the vessel
+        patch_groundtruth=np.where(patch_groundtruth>0,255,0)
+
+        #patch_image_binary =cv2.equalizeHist((patch_image_binary*255.0).astype(np.uint8))/255.0
+
+        patch_image_list.append(patch_image_binary)    # patch image
+        patch_groundtruth_list.append(patch_groundtruth)             # patch mask
+        if show:
+          cv2.rectangle(image_show, (y-patch_size//2,x-patch_size//2,), (y+patch_size//2,x+patch_size//2), (0,1,0), 2)  #draw the illustration
+          cv2.rectangle(groundtruth_show, (y-patch_size//2,x-patch_size//2,), (y+patch_size//2,x+patch_size//2), (0,1,0), 2)
+        sample_count+=1
+
+    if show:                                 # visualize the sample process
+      plt.figure(figsize=(15,15))
+      plt.title("processing: %s"%image_name)
+      plt.subplot(121)
+      plt.imshow(image_show,cmap=plt.cm.gray)   # processd image
+      plt.subplot(122)
+      plt.imshow(groundtruth_show,cmap=plt.cm.gray)  #groundtruth of the image, patch is showed as the green square
+      plt.show()
+      display.clear_output(wait=True)
+    sample_index+=1
+
+  for i in range(len(patch_image_list)):
+    if training==True:
+        plt.imsave(train_patch_dir+image_name+"-"+str(i)+"-img.jpg",patch_image_list[i])
+        #print(patch_mask_list[i])
+        plt.imsave(train_patch_dir+image_name+"-"+str(i)+"-groundtruth.jpg",(patch_groundtruth_list[i]/225.0).astype(np.uint8),cmap = plt.cm.gray)
+    else:
+        plt.imsave(train_patch_dir+image_name+"_"+str(i)+"_val_img.jpg",patch_image_list[i])
+        #print(patch_mask_list[i])
+        plt.imsave(train_patch_dir+image_name+"_"+str(i)+"_val_groundtruth.jpg",(patch_groundtruth_list[i]/225.0).astype(np.uint8),cmap = plt.cm.gray)
